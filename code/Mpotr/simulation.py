19e2731b635582245460137a1a1e4ee13d43feea
jtcorcoran <jtcorcoran7331@gmail.com>
2014-04-11 00:03:58 -0400

    Added sim

--- code/Mpotr/simulation.py
+++ code/Mpotr/simulation.py
@@ -7,36 +7,42 @@ from multiprocessing import Process
 
 
 class Simulation():
-	def __init__():
-		self.q = Queue()
+	def __init__(self):
+		self.q = Queue.Queue()
 		self.users = 4
 		self.userlist = []
 		pass
 		
-	def Start():
+	def Start(self):
 		
-		for u in users:
+		server = Server()
+		for u in range(0, self.users):
 			user = User()
-			userlist.append(u)
+			self.userlist.append(user)
 		
-		userlist[0].setup()
 		
-	def setup():
+		self.userlist[0].setup()
 		
 
 class Server():
 	def __init__(self):
-		self.q = Queue()
+		q = Queue.Queue()
+		pass
+
+	def relay(self, receive, send):
 		pass
 	
 class Message():
 
 	def __init__(self):
-		msgLength = random.randint(2, 200) #length of the message
+		msgLength = random.randint(2, 200) #length of the message in bytes
 		
 class User():
 	def __init__(self):
 		self.distance = random.randrange(20, 400)
+
+	def setup(self):
+		print "YESS"
 
 def test():
 	print "Hello world"
