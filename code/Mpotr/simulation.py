7c2ab96550be9e550d75473582cb8d6116771d0c
jtcorcoran <jtcorcoran7331@gmail.com>
2014-04-16 22:02:58 -0400

    Latest

--- code/Mpotr/simulation.py
+++ code/Mpotr/simulation.py
@@ -4,6 +4,7 @@
 
 import Queue, random, multiprocessing
 from multiprocessing import Process
+from multiprocessing.dummy import Pool as ThreadPool
 
 
 class Simulation():
@@ -15,22 +16,34 @@ class Simulation():
 		
 	def Start(self):
 		
-		server = Server()
-		for u in range(0, self.users):
-			user = User()
+		self.server = Server()
+		for u in range(0, int(self.users)):
+			user = User("user"+str(u))
 			user.responseProb = 1.0/self.users
 			self.userlist.append(user)
-		
 		
 		self.userlist[0].setup(self)
 		
 
 class Server():
 	def __init__(self):
-		q = Queue.Queue()
+		self.q = Queue.Queue()
 		pass
 
-	def relay(self, receive, send):
+	def relay(self, receive, send, msg):
+		pass
+
+	def check(self, sim):
+		while not self.q.empty():
+			(x,y) = self.q.get_nowait()
+			for user in sim.userlist:
+				if not x == user:
+		#			this.relay(
+					print x
+			print y
+					
+
+	def Start():
 		pass
 	
 class Message():
@@ -38,24 +51,35 @@ class Message():
 	def __init__(self):
 		msgLength = random.randint(2, 200) #length of the message in bytes
 		
-class User():
-	def __init__(self):
+class User(multiprocessing.Process):
+	def __init__(self, name):
 		self.distance = random.randrange(20, 400)
 		self.responseProb = None
+		self.q = Queue.Queue()
+		self.name = name
 
 	def setup(self, sim):
-		self.Broadcast(sim.users, "hey")
+		self.Broadcast(sim, "0x12user")
 		
-	def Broadcast(self, participants, msg):
-		print msg
+	def Broadcast(self, sim, msg):
+		for p in sim.userlist:
+			if not self == p:
+				# add messages to server queue
+				sim.server.q.put((self, msg))		
+		sim.server.check(sim)
+
+	def Receive(self, msg):
+		pass
 
 def test():
 	print "Hello world"
 
 def main():
+#	pool = ThreadPool(4)
 	sim = Simulation()
 	sim.Start()
 
 if __name__ == '__main__':
 	main()
-		
+
+
